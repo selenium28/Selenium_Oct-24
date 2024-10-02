4a116a7e8d1e9773b94c3bef30a51a94b25f8b62
selenium28 <devselenium.class2819@gmail.com>
2024-10-02 20:46:35 +0530

    Locators

--- src/core/SeleniumClass.java
+++ src/core/SeleniumClass.java
@@ -1,9 +1,23 @@
 package core;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class SeleniumClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+//		WebDriver - Interface in Selenium
+//		driver - reference variable
+//		new - keyword
+//		ChromeDriver - Class
+//		new ChromeDriver(); - Object
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.selenium.dev/documentation/");
+		
+//		WebDriver driver1 = new WebDriver(); -> we can't use like this 
+//		ChromeDriver driver2 = new ChromeDriver();
+		
 
 	}
 
